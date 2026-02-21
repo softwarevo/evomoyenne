47ea631776cf10be21a7bfea90d8b6f86bd40619
Adouche <adam@evomoyenne.qzz.io>
2026-02-21 11:21:22 +0100

    Refresh cache

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('./assets/js/idb.js');
 
-const CACHE_NAME = 'evomoyenne-v1.2-refreshtest2';
+const CACHE_NAME = 'evomoyenne-v1.2-refreshtest3';
 const ASSETS = [
     './',
     './index.html',
